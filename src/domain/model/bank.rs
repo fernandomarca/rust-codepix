a98b7b1af06770292b15b6a9bf1638859468b60f
Fernando Marca Magalhães <fernandomarca@hotmail.com>
2022-04-05 02:43:50 -0300

    validate bank model

--- src/domain/model/bank.rs
+++ src/domain/model/bank.rs
@@ -1,16 +1,30 @@
 use chrono::Utc;
+use serde::Deserialize;
 use uuid::Uuid;
-struct Bank {
+use validator::{Validate, ValidationErrors};
+#[derive(Debug, Validate, Deserialize)]
+pub struct Bank {
+  // #[validate(uuid = "lower")]
+  #[validate(length(min = 1))]
   id: String,
+
+  #[validate(length(min = 1))]
+  #[serde(rename = "createdAt")]
   created_at: String,
+
+  #[validate(length(min = 1))]
+  #[serde(rename = "updatedAt")]
   updated_at: String,
+
+  #[validate(length(min = 1))]
   code: String,
+  #[validate(length(min = 1))]
   name: String,
   // Accounts []*Account `valid:"_"`
 }
 
 impl Bank {
-  fn new(code: String, name: String) -> Bank {
+  pub fn new(code: String, name: String) -> Result<Bank, ValidationErrors> {
     let bank = Bank {
       id: Uuid::new_v4().to_string(),
       code,
@@ -18,7 +32,11 @@ impl Bank {
       created_at: Utc::now().to_string(),
       updated_at: Utc::now().to_string(),
     };
+    bank.is_valid()?;
+    Ok(bank)
+  }
 
-    bank
+  fn is_valid(&self) -> Result<(), ValidationErrors> {
+    self.validate()
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 pub mod domain;
-
+use domain::model::bank::Bank;
 fn main() {
-    // let bank = Bank::new();
-    println!("Code Pix!");
+    let bank = Bank::new("b01".to_string(), "bank01".to_string());
+    match bank {
+        Ok(bank) => println!("{:?}", bank),
+        Err(err) => println!("{:?}", err),
+    }
 }
