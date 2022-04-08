9cca5433e0ba72c5461303fc99a39fccce1f8979
Fernando Marca Magalhães <fernandomarca@hotmail.com>
2022-04-08 00:14:19 -0300

    integrated prisma

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,46 @@
 mod db;
 pub mod domain;
-use db::{PrismaClient, User};
+use std::vec;
+
+use db::{Post, PrismaClient, User};
 pub use domain::model::{account::Account, bank::Bank, pix_key::PixKey};
 
 #[tokio::main]
 async fn main() {
     let client = PrismaClient::new().await;
-
     // Required values are passed in as separate arguments
     let user = client
         .user()
         .create_one(
-            User::id().set("user3".to_string()),
+            User::username().set("user4".to_string()),
             User::display_name().set("User 0".to_string()),
-            // Optional arguments can be added in a Vec as the last parameter
             vec![],
         )
         .exec()
         .await;
 
-    print!("{:?}", user);
+    let post = client
+        .post()
+        .create_one(
+            Post::id().set("0".to_string()),
+            Post::content().set("Some post content".to_string()),
+            Post::user().link(User::username().equals(user.username.to_string())),
+            vec![],
+        )
+        .exec()
+        .await;
+
+    print!("{:?},{:?}", user, post);
+    // let user = client
+    //     .user()
+    //     .find_many(vec![
+    //         User::display_name().contains("u".to_string()),
+    //         User::id().contains("3".to_string()),
+    //     ])
+    //     .exec()
+    //     .await;
+
+    // print!("{:?}", user);
     // Find all records that match some parameters
     // let unique_user = client
     //     .user()
